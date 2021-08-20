0516725cdbadab9ec3e58affd675b426829012e7
Sebastián Saldaña <sebas@Sebastians-Mac-mini.local>
2021-08-19 21:14:58 -0500

    deploy

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-
-const path = require('path');
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-// Serve static files
-app.use(express.static(__dirname + '/dist/cartas'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/cartas'));
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/src/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/cartas/'}),
+);
 
-// default Heroku port
-app.listen(process.env.PORT || 5000);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
