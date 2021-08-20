adebafa4b746c8d070e83c928e7bf903c7eb626f
Sebastián Saldaña <sebas@Sebastians-Mac-mini.local>
2021-08-19 20:07:08 -0500

    initial heroku deploy

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/cartas'));
+app.use(express.static(__dirname));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/cartas/index.html'));
+  res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 
 // default Heroku port
