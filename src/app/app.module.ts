f6d25eb73264dda4af921fc5d1a14606bfe5f1a0
Sebastián Saldaña <sebas@Sebastians-Mac-mini.local>
2021-08-19 19:31:37 -0500

    make it better

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReadexcelDirective } from './directives/readexcel.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReadexcelDirective
   ],
   imports: [
     BrowserModule
